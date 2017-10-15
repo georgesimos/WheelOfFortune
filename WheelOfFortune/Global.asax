89f6defff47f3c6bc727919aaade5c1d548851e0
DESKTOP-MG26L5N\George Simos <admin@georgesimos.com>
2017-10-15 11:59:15 +0300

    Add project files.

---
+++ WheelOfFortune/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WheelOfFortune.MvcApplication" Language="C#" %>
---
+++ WheelOfFortune/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WheelOfFortune.Startup))]
+namespace WheelOfFortune
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
